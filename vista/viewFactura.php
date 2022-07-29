77e8f4051ce2d34221b5fad7b72d31226a532c79
jgenoss <110179996+jgenoss@users.noreply.github.com>
2022-07-28 21:02:17 -0500

    etc

--- vista/viewFactura.php
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">
-var base64 = "";
-let pdfWindow = window.open("");
-pdfWindow.document.write("<iframe width='100%' height='100%' src='data:application/pdf;base64, " + base64 + "'></iframe>");
-</script>
