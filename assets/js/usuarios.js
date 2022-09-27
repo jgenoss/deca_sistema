fc642ea3e538e649c9c0dc0866384430c343fd08
JGenoss <granadillo33@gmail.com>
2022-09-27 17:21:56 -0500

    add

--- assets/js/usuarios.js
+++ assets/js/usuarios.js
@@ -2,13 +2,34 @@ new Vue({
 	el:'#usuarios',
 	data(){
 		return{
-
+			data:{
+				login:'',
+				password:'',
+				name:'',
+				status:''
+			},
+			list:true
 		}
 	},
 	created(){
 
 	},
 	methods:{
-
+		submit:function() {
+			axios.post('controlador/usuarios.php?op=new',this.data)
+				.then(resp =>{
+					console.log(resp.data);
+			})
+		},
+		newbutton(){
+			this.form = false;
+			this.resetInput();
+		},
+		resetInput(){
+			this.data.login='';
+			this.data.password='';
+			this.data.name='';
+			this.data.status='';
+		}
 	}
 })
--- controlador/usuarios.php
+++ controlador/usuarios.php
@@ -5,18 +5,31 @@ require_once '../includes/config.php';
 $_POST = json_decode(file_get_contents("php://input"), true);
 
 require_once '../modelo/dbconnect.php';
+$db = new dbconnect();
+
 switch (@$_GET['op']) {
 	case 'listUsers':
-		{	
+		{
 			if (isset($_GET)) {
 				echo "string";
 			}
-		}	
+		}
 		break;
-	case 'addUser':
+	case 'new':
 		{
 			if (isset($_POST)) {
-				var_dump($_POST);
+				$A = array(
+					$_POST['login'],
+					$_POST['password'],
+					$_POST['name'],
+					$_POST['status']
+				);
+				try {
+					$db->sql("INSERT INTO usuario(usuario, contrasena, nombre, habilitado)VALUES('$A[0]','$A[1]','$A[2]','$A[3]')");
+					setMsg("ALERT","Usuario regisatrdo con exito","success");
+				} catch (Exception $e) {
+					setMsg("Error",$e->getMessage(),"error");
+				}
 			}
 		}
 		break;
